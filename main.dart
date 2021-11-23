798a09159b4f6c8186e39b5ed39241cbdcbbd5ce
Ashrafi Abir <90349666+ashrafiabir01@users.noreply.github.com>
2021-11-23 13:37:54 +0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,25 @@
+import 'package:calculator_google/screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'demo.dart';
+void main () {
+  runApp(mainpage());
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Color(0xFFCDE6E6)
+  ));
+
+}
+
+class mainpage extends StatelessWidget {
+
+  const mainpage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: screena(),
+    );
+  }
+}
